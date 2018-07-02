806973f800697366bd3577b305ad700992464965
Yukito Ito <yukibukiyou@gmail.com>
2018-07-02 18:25:16 +0900

    fix test

--- fish_uniq_history_test.go
+++ fish_uniq_history_test.go
@@ -6,7 +6,7 @@ import (
 
 var testFile = "test_history"
 
-var expectCommandList = [12]string{"brew install ghq", "docker --version", "ghq get https://github.com/ykpythemind/dotfiles.git", "brew doctor", "sh build.sh ", "chsh", "sudo vim /etc/shells", "man curl", "sh init.sh ", "vim .zshrc", "vim .zshenv "}
+var expectCommandList = [12]string{"brew install ghq", "docker --version", "ghq get https://github.com/ykpythemind/dotfiles.git", "brew doctor", "sh build.sh ", "docker --version", "sudo vim /etc/shells", "man curl", "sh init.sh ", "vim .zshrc", "vim .zshenv "}
 
 func TestRead(t *testing.T) {
 	list := Read(testFile)
@@ -31,7 +31,7 @@ func TestUniq(t *testing.T) {
 	dupList := []string{"a", "aaa", "a", "piyo", "hoge", "hoge", "a", "piyo2", "piyo", "fuga"}
 	expectList := []string{"a", "aaa", "piyo", "hoge", "piyo2", "fuga"}
 	uniqedList := Uniq(dupList)
-	for i := 0; i < len(expectList)-1; i++ {
+	for i := 0; i < len(expectList); i++ {
 		if expectList[i] != uniqedList[i] {
 			t.Errorf("failed: expect %s, but got %s", expectList[i], uniqedList[i])
 		}
@@ -39,10 +39,10 @@ func TestUniq(t *testing.T) {
 }
 
 func TestReverse(t *testing.T) {
-	list := []string{"1", "2", "4", "5", "3"}
-	expectList := []string{"3", "5", "4", "2", "1"}
+	list := []string{"1", "2", "4", "a", "5", "3"}
+	expectList := []string{"3", "5", "a", "4", "2", "1"}
 	Reverse(list)
-	for i := 0; i < len(expectList)-1; i++ {
+	for i := 0; i < len(expectList); i++ {
 		if expectList[i] != list[i] {
 			t.Errorf("failed: expect %s, but got %s", expectList[i], list[i])
 		}
